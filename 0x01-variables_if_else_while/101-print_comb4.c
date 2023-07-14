6e46440a2bc2a9515a3d77fb8d78a1a64d97195d
Albtiui <mohamedalbtiui2022@gmail.com>
2023-07-14 04:54:21 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,7 +4,6 @@
  * main - prints all possible different combinations of three digits
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 int n, m, l;
@@ -25,11 +24,10 @@ if (n != 55 || m != 56)
 putchar(',');
 putchar(' ');
 }
-}                        
+}
 }
 }
 }
 putchar('\n');
-
 return (0);
 }
