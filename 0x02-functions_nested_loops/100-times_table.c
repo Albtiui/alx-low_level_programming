3ea45aab21c2e60d39d15a287e862240353b1be4
Albtiui <mohamedalbtiui2022@gmail.com>
2023-07-18 12:47:17 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,15 +24,15 @@ if (op <= 9)
 _putchar(32);
 _putchar(32);
 _putchar(op + 48);
-}
+{
 else if (op <= 99)
 {
 _putchar(32);
 _putchar((op / 10) + 48);
 _putchar((op % 10) + 48);
-}
-else
 {
+else
+}
 _putchar(((op / 100) % 10) + 48);
 _putchar(((op / 10) % 10) + 48);
 _putchar((op % 10) + 48);
